630656c55c34a90aaeb1449ace0c350e03b93670
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2024-11-01 14:47:56 +0800

    chore: update

--- auto-update-lobe-chat.sh
+++ auto-update-lobe-chat.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
- 
+
+LOBE_IMAGE=${LOBE_IMAGE:-"lobehub/lobe-chat:latest"}
+
 # Pull the latest image and store the output in a variable
-output=$(docker pull lobehub/lobe-chat:latest 2>&1)
+output=$(docker pull $LOBE_IMAGE 2>&1)
  
 # Check if the pull command was executed successfully
 if [ $? -ne 0 ]; then
@@ -9,7 +11,7 @@ if [ $? -ne 0 ]; then
 fi
  
 # Check if the output contains a specific string
-echo "$output" | grep -q "Image is up to date for lobehub/lobe-chat:latest"
+echo "$output" | grep -q "Image is up to date for $LOBE_IMAGE"
  
 # If the image is already up to date, do nothing
 if [ $? -eq 0 ]; then
@@ -29,8 +31,8 @@ echo "Started: $(docker-compose up -d)"
  
 # Print the update time and version
 echo "Update time: $(date)"
-echo "Version: $(docker inspect lobehub/lobe-chat:latest | grep 'org.opencontainers.image.version' | awk -F'"' '{print $4}')"
+echo "Version: $(docker inspect $LOBE_IMAGE | grep 'org.opencontainers.image.version' | awk -F'"' '{print $4}')"
  
 # Clean up unused images
-docker images | grep 'lobehub/lobe-chat' | grep -v 'latest' | awk '{print $3}' | xargs -r docker rmi > /dev/null 2>&1
+docker images | grep $LOBE_IMAGE | grep -v 'latest' | awk '{print $3}' | xargs -r docker rmi > /dev/null 2>&1
 echo "Removed old images."
